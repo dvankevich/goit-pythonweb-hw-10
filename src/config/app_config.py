3e3db422d61cfd4c5cb098b858e46d1349dbc62a
dvankevich <dvankevich@gmail.com>
2026-04-23 21:58:47 +0300

    fix app_config

--- src/config/app_config.py
+++ src/config/app_config.py
@@ -1,31 +1,16 @@
-from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-# Визначаємо шлях до кореня проекту (де лежить .env)
-# Path(__file__).resolve() — це шлях до поточного файлу settings.py
-# .parent.parent — піднімаємось на рівні вгору до кореня
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 class Settings(BaseSettings):
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_DB: str
-    POSTGRES_HOST: str
-    POSTGRES_PORT: int
+    DATABASE_URL: str
     LOG_LEVEL: str = "INFO"
 
-    @property
-    def DATABASE_URL(self) -> str:
-        return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
-
     model_config = SettingsConfigDict(
-        # Використовуємо абсолютний шлях
-        env_file=BASE_DIR / ".env",
+        env_file=".env",
         env_file_encoding="utf-8",
-        extra="ignore",
+        extra="ignore", 
+        case_sensitive=True
     )
 
 
-
-settings = Settings()
-
+settings = Settings() # type: ignore
