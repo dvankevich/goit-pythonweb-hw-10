d9eccdf0b497f83a3011ef5c6acc41d84b04ca83
dvankevich <dvankevich@gmail.com>
2026-04-27 19:54:43 +0300

    add ratelimit to /api/users/me enpoint

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import uvicorn
-from fastapi import FastAPI, Depends, HTTPException, status 
+from fastapi import FastAPI, Depends, HTTPException, status, Request
+from starlette.responses import JSONResponse
+from slowapi.errors import RateLimitExceeded
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 from src.db.session import get_db
 from src.api.contact_api import router as contact_router
 from src.api import auth, users
+from src.api.users import limiter
 
 app = FastAPI(title="Contacts API")
+app.state.limiter = limiter
+
+
+@app.exception_handler(RateLimitExceeded)
+async def rate_limit_handler(request: Request, exc: RateLimitExceeded):
+    return JSONResponse(
+        status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+        content={"error": "Перевищено ліміт запитів. Спробуйте пізніше."},
+    )
+
 
 app.include_router(contact_router, prefix="/api")
 app.include_router(auth.router, prefix="/api")
@@ -38,5 +51,6 @@ app.include_router(users.router, prefix="/api")
 def root():
     return {"message": "Welcome to Contacts API. Go to /docs for Swagger UI."}
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
